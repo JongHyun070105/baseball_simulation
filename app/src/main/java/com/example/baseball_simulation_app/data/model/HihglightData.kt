648a08cd0a08ac8c6df5330471ab559ee953a01e
JongHyun070105 <bobi09361@gmail.com>
2025-04-10 07:49:30 +0900

    SBO

---
+++ app/src/main/java/com/example/baseball_simulation_app/data/model/HihglightData.kt
@@ -0,0 +1,19 @@
+package com.example.baseball_simulation_app.data.model
+
+data class HighlightData(
+    val inning: String,
+    val homeTeamName: String,
+    val homeTeamLogoRes: Int,
+    val awayTeamName: String,
+    val awayTeamLogoRes: Int,
+    val batterName: String,
+    val pitcherName: String,
+    val baseStatus: BaseStatus,
+    val outCount: Int,
+)
+
+data class BaseStatus(
+    val first: Boolean = false,
+    val second: Boolean = false,
+    val third: Boolean = false
+)
